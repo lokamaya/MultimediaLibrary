bb4a1e0e112e1f57257c6d460c21938c42e3a1bd
Zaenal <public@lokamaya.com>
2016-01-27 06:45:21 +0700

    Update schema: 0.2.0-alpha
    Adding some tables

--- core/components/multimedialibrary/model/multimedialibrary/metadata.mysql.php
+++ core/components/multimedialibrary/model/multimedialibrary/metadata.mysql.php
@@ -1,11 +1,30 @@
 <?php
-
+/**
+ * MultimediaLibrary for MODX Revolution
+ *
+ * @package multimedialibrary
+ * @author Zaenal/Lokamaya <zaenal[#]lokamaya.com>
+ */
+/**
+ * @staticvar array $xpdo_meta_map for mapping related table
+ */
 $xpdo_meta_map = array (
   'xPDOSimpleObject' => 
   array (
     0 => 'MultimediaCollection',
-    1 => 'MultimediaCollectionInfo',
+    1 => 'MultimediaCollectionComment',
     2 => 'MultimediaContent',
     3 => 'MultimediaContentVersion',
+    4 => 'MultimediaPlaylist',
+    5 => 'MultimediaPlaylistItems',
+    6 => 'MultimediaPlaylistComment',
+    7 => 'MultimediaUserHistory',
+  ),
+  'xPDOObject' => 
+  array (
+    0 => 'MultimediaCollectionInfo',
+    1 => 'MultimediaCollectionLiked',
+    2 => 'MultimediaCollectionTag',
+    3 => 'MultimediaPlaylistLiked',
   ),
 );
