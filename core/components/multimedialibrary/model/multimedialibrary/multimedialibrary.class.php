2a17a18b46d0af2d27e0c6dba08ec629abcfba0b
Zaenal <public@lokamaya.com>
2016-01-27 06:52:56 +0700

    Adding baseClass MultimediaLibrary

---
+++ core/components/multimedialibrary/model/multimedialibrary/multimedialibrary.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * MultimediaLibrary for MODX Revolution
+ *
+ * @package multimedialibrary
+ * @author Zaenal/Lokamaya <zaenal[#]lokamaya.com>
+ */
+/**
+ * class MultimediaLibrary
+ * base class for this package
+ */
+class MultimediaLibrary {}
